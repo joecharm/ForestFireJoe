52cd6b81bce83ae84c35348b117fccf57ca1bf60
Joe <919946@swansea.ac.uk>
2020-11-06 10:43:17 +0000

    Added fix to allow project to build

--- Assets/ForestFire/Scripts/TriggerFlare.cs
+++ Assets/ForestFire/Scripts/TriggerFlare.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UIElements;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
